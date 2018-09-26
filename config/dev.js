dda9d0474d915cc0d5c9f7166c5347384ca8461b
Caio Blasio <caioblasio@ZMAC030.local>
2018-09-26 20:08:25 -0300

    project scaffold

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:123456Ab@ds161443.mlab.com:61443/dev-shareknowledge'
+};
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true } );
+
+const app = express();
+app.use(bodyParser.json());
+
+require('./routes/routes')(app);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+
+if (isProduction) {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function(){
+  console.log('server running....2')
+});
